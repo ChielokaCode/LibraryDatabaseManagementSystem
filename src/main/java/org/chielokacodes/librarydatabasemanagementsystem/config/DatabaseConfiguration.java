f7f2e0a21e5d0501036a971cef2dd1de299294d2
ChielokaCode <realmatec01@gmail.com>
2024-05-24 08:20:27 +0100

    initial commit

--- src/main/java/org/chielokacodes/librarydatabasemanagementsystem/config/DatabaseConfiguration.java
+++ src/main/java/org/chielokacodes/librarydatabasemanagementsystem/config/DatabaseConfiguration.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 public class DatabaseConfiguration {
     private final String DB_URL = "jdbc:mysql://localhost:3306/libManagementDB";
     private final String DB_USERNAME = "root";
-    private final String DB_PASSWORD = "DivineMercy123";
+    private final String DB_PASSWORD = "";
     private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 }
